df3d822470f27c29bb05ecf4aea9fe55ce9d1767
roshanr <roshanranabhat11@gmail.com>
2017-11-08 08:12:04 +0545

    authentication failure handler added

--- src/main/java/com/music/cms/security/SimpleAuthenticationFailureHandler.java
+++ src/main/java/com/music/cms/security/SimpleAuthenticationFailureHandler.java
@@ -3,14 +3,12 @@ package com.music.cms.security;
 import com.music.cms.dao.UserDetailsDao;
 import com.music.cms.model.UserAttempts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
